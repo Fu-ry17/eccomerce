3ac2ef9f8a8daf86f9faa34041fc07349b024be9
Fu-_ry17 <yewabrian17@gmail.com>
2022-02-23 01:44:01 +0300

    updated orders

--- wanda/src/pages/order/[slug].tsx
+++ wanda/src/pages/order/[slug].tsx
@@ -1,6 +1,7 @@
+import { Image } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Loading from '../../components/alert/Loading'
 import { ALERT } from '../../redux/types/alertTypes'
 import { getAPI } from '../../utils/fetchData'
@@ -46,6 +47,7 @@ function OrderDetails() {
           <h3> Delvery-Location : {order.location} </h3>
           <h4> Payment-Method : {order.paymentMethod}</h4>
           <h5> Paid : {order.paid ? 'Paid' : 'Not Paid' } </h5>
+          <span> Ordred On : {new Date(order.createdAt as Date).toLocaleDateString()}</span>
         </div>
 
         <h1 className='text-2xl font-bold my-6'>ksh {total.toFixed(2)}</h1>
@@ -53,7 +55,32 @@ function OrderDetails() {
         <div className='my-8 p-2 bg-red-400 text-center text-white'>
           { order.status ? 'Delivered' : 'Not Delivered'}
         </div>
+                    
+        <div>
+          {
+            order.cart?.map(item =>(
+                <div key={item._id} className="flex gap-4 px-2 items-center mb-4 w-full">
+                     <Image src={item.images[0].url} 
+                     className='rounded-md w-full max-w-[80px] max-h-[80px] object-cover object-top block shadow-sm hover:shadow-lg overflow-hidden'/> 
+                     
+                     <div className='flex flex-wrap justify-between w-full items-center'>
+                      <div>
+                        <Link to={`/shop/${item.slug}`} className="w-full">
+                          <h3 className='pb-3 font-bold text-md'>{item.title}</h3>
+                        </Link>
+                        <span> quantity : 1 * {item.qty} </span>
+                      </div>
 
+                      <div className='font-semibold'>
+                        ksh: {(item.price as number) * (item.qty as number)}.00
+                      </div>
+                 
+                     </div>
+                   
+                </div>
+            ))
+          }
+        </div>
     </div>
   )
 }
