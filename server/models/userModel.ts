f3bf592931f0a620d0eafe40c453baf0e23e462f
Fu-_ry17 <yewabrian17@gmail.com>
2022-03-01 14:51:45 +0300

    updated

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -30,6 +30,10 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
+    wishList: {
+       type: Array,
+       default: []
+    },
     subscription: {
         type: Array,
         default: []
